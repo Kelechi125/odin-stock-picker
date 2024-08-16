47714dfff211f1165a7be4b0c1535c3c9169aa2f
Kelechi125 <kelechi125@yahoo.com>
2024-08-16 10:56:27 -0500

    Updated the nested loop using array iterator methods instead of the for loop

--- script.rb
+++ script.rb
@@ -13,23 +13,8 @@
 
 def stock_picker(array)
 
-=begin
-  buying_price = 0
-  selling_price = 0
-
   array.each_with_index do |price, day|
-     # puts "On Day #{day}, the price is $#{price}."
-     if day === 0
-      next
-     elsif
-      
-     
-  end
-=end
-
-  for price in array do
-    for price_2 in array do
-
+    array.each_with_index do |price_2, day_2|
       if price > price_2
         pair = []
         pair.push(price, price_2)
@@ -38,10 +23,11 @@ def stock_picker(array)
         p pair
         p difference
       end
-      
     end
+      
+     
   end
-  
+
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
