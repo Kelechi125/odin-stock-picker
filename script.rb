8272472b591835b185805121cbb0b5a925bed2c8
Kelechi125 <kelechi125@yahoo.com>
2024-08-16 10:35:22 -0500

    Created nested for loops to create pairings of the stock prices and calculate differences

--- script.rb
+++ script.rb
@@ -3,13 +3,43 @@
 # And the best day to sell
 # Need to buy before selling
 
+# Pay attention to edge cases like when the lowest day is the last day
+# And the highest day is the first day
+
 # Want access to the indices as they represent the days starting at Day 0
 # Iterate through the array and compare each element to the one before and after
+# Maybe check out the array.max() and array.min() methods
 
 
 def stock_picker(array)
+
+=begin
+  buying_price = 0
+  selling_price = 0
+
   array.each_with_index do |price, day|
-    puts "On Day #{day}, the price is $#{price}."
+     # puts "On Day #{day}, the price is $#{price}."
+     if day === 0
+      next
+     elsif
+      
+     
+  end
+=end
+
+  for price in array do
+    for price_2 in array do
+
+      if price > price_2
+        pair = []
+        pair.push(price, price_2)
+        difference = price - price_2
+
+        p pair
+        p difference
+      end
+      
+    end
   end
   
 end
